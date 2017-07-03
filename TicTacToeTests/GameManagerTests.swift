bfdec769242ba671d9c4adab428f67fae565d04b
Romilson Nunes <souzainf3@yahoo.com.br>
2017-07-03 10:34:01 -0300

    Game manager test victory

--- TicTacToeTests/GameManagerTests.swift
+++ TicTacToeTests/GameManagerTests.swift
@@ -54,4 +54,34 @@ class GameManagerTests: XCTestCase {
         XCTAssertNil(manager.lastPlayer)
     }
     
+    func testGameVictory() {
+        // Row
+        let point0 = BoardPoint(column: 0, row: 0)
+        let point1 = BoardPoint(column: 1, row: 0)
+        let point2 = BoardPoint(column: 2, row: 0)
+
+        manager.select(point: point0, from: player1)
+        manager.select(point: point1, from: player1)
+        manager.select(point: point2, from: player1)
+
+        XCTAssertFalse(manager.checkVictory(from: player2))
+        XCTAssertTrue(manager.checkVictory(from: player1))
+        
+        
+        // Diagonal - Inverted Player
+        manager.reset()
+        
+        let point3 = BoardPoint(column: 0, row: 0)
+        let point4 = BoardPoint(column: 1, row: 1)
+        let point5 = BoardPoint(column: 2, row: 2)
+        
+        manager.select(point: point3, from: player2)
+        manager.select(point: point4, from: player2)
+        manager.select(point: point5, from: player2)
+        
+        XCTAssertFalse(manager.checkVictory(from: player1))
+        XCTAssertTrue(manager.checkVictory(from: player2))
+        
+    }
+    
 }
